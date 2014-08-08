455d3a5bcaa6efda4d47f468ea4da6212e90d029
John Høj Andreassen <jobbi_john@hotmail.com>
2014-08-08 09:40:24 +0100

    Chat connected to MongoDB, it will save all messages which are not private to the message collection

--- app.js
+++ app.js
@@ -2,24 +2,57 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app), //Create server object
     io = require('socket.io').listen(server), //Socket functionality which listens to the http server object
+    mongoose = require('mongoose'),
     // Keeping track of nicknames as objects, as well as being able to reference the single socket object so we can send to specific user sockets.
     // Nickname will be the key to the users object and the values will be the sockets
     users = {};
 
-
 // Listen to port 3000
 server.listen(3000);
+
+//Database connect
+mongoose.connect('mongodb://localhost/nodeChat', function(err){
+    if(err){
+        console.log(err);
+    } else {
+        console.log('DB connection successfull');
+    }
+});
+
+// Creating a schema
+var chatSchema = mongoose.Schema({
+    nick: String,
+    msg: String,
+    created: {type: Date, default: Date.now}
+});
+
+// Create model
+// Keep name singular -> it will be turned prural
+// 1. parameter name of collection/model
+// 2. parameter is the schema of the collection
+var Chat = mongoose.model('Message', chatSchema);
+
+
 
 // Create route with express
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-
 // Receive event (server-side)
 // Like document ready function
 // Every user has it's own socket -> (function(socket)).
 io.sockets.on('connection', function(socket){
+
+    //Grabb earlier messages when logged on
+    Chat.find({}, function(err, docs){
+        if(err){
+            throw err;
+        } else {
+            console.log('Sending old messages');
+            socket.emit('load old message', docs);
+        }
+    });
 
     //Listening for new user event
     //Callback parameter to send valid state data back to client
@@ -27,15 +60,13 @@ io.sockets.on('connection', function(socket){
     socket.on('new user', function(data, callback){
 
         //If nickname is available
-        if(data in users){
+        if (data in users){
             callback(false);
-        } else {
+        } else{
             callback(true);
             socket.nickname = data; //Saving the nickname to the socket it self -> now it is a property of the socket
             users[socket.nickname] = socket;
             updateNicknames(); // Sending updated array with nicknames to all sockets
-
-            console.dir(users);
         }
     });
 
@@ -47,43 +78,47 @@ io.sockets.on('connection', function(socket){
     socket.on('send message', function(data, callback){
         // Private message "listener".
         var msg = data.trim(); // trimming if user has put white space as first characters
+        console.log('after trimming message is: ' + msg);
         if(msg.substr(0,3) === '/w '){
             msg = msg.substr(3); // We need the next letters
             var ind = msg.indexOf(' ');
             if(ind !== -1){
                 var name = msg.substring(0, ind); // Looking for first white space as after that the name should be finished
                 var msg = msg.substring(ind + 1);
-
                 if(name in users){
                     //emit using the socket we are sending the message to
-                    users[name].emit('whisper', {msg: msg, nick: socket.nickname})
+                    users[name].emit('whisper', {msg: msg, nick: socket.nickname});
                     // If user has entered /w then it is a private message
                     // this will later be changed to better listener
-                    console.log("Whisper to: " );
-                } else {
-                    callback('Enter a valid user')
+                    console.log('message sent is: ' + msg);
+                    console.log('Whisper!');
+                } else{
+                    //Handling data from send message
+                    //We are sending it to all users here
+                    callback('Error!  Enter a valid user.');
                 }
-
-            } else {
-                callback('Error! Please enter a message for you whisper')
+            } else{
+                callback('Error!  Please enter a message for your whisper.');
             }
-
-        } else {
-            //Handling data from send message
-            //We are sending it to all users here
+        } else{
+            // Create new document
+            // Have set date to default now so we do not need to mention that
+            var newMsg = new Chat({msg: msg, nick: socket.nickname});
+            //Callback function for error
+            newMsg.save(function(err){
+                if(err){
+                    throw err;
+                }
+            });
             io.sockets.emit('new message', {msg: msg, nick: socket.nickname});
         }
-
     });
-
 
     //When a user disconnects
     socket.on('disconnect', function(data){
         //To disconnect them if they wisit the site, but do not enter anything we want to disconnect them from the socket.
-        console.log(socket.nickname + ": User disconnected");
         if(!socket.nickname) return;
         delete users[socket.nickname];
-        console.log("On disconnect: " + users);
         updateNicknames(); // Send the new list to all sockets so get the new list of users
     });
 });
