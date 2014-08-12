ef729172673c4b90d5b070cd067cf81b811d4bc0
John Høj Andreassen <jobbi_john@hotmail.com>
2014-08-12 15:55:06 +0100

    Functioning chat app with private messaging. When user is selected the private conversation will be displayed.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express'),
     server = require('http').createServer(app), //Create server object
     io = require('socket.io').listen(server), //Socket functionality which listens to the http server object
     mongoose = require('mongoose'),
-    crypto = require('crypto'),
     // Keeping track of nicknames as objects, as well as being able to reference the single socket object so we can send to specific user sockets.
     // Nickname will be the key to the users object and the values will be the sockets
     users = {};
@@ -56,20 +55,16 @@ app.get('/', function(req, res){
 // Like document ready function
 // Every user has it's own socket -> (function(socket)).
 io.sockets.on('connection', function(socket){
-    //Create a random id for socket
-    var cryptoID = crypto.randomBytes(20).toString('hex');
-    console.log(cryptoID);
 
     //Grabb earlier messages when logged on
-    query = Chat.find({});
+    /*var query = Chat.find({});
     query.sort('-created').limit(8).exec(function(err, docs){
         if(err){
             throw err;
         } else {
-            console.log('Sending old messages');
             socket.emit('load old message', docs);
         }
-    });
+    });*/
 
     //Listening for new user event
     //Callback parameter to send valid state data back to client
@@ -97,11 +92,15 @@ io.sockets.on('connection', function(socket){
 
             //If user is in database
             if(user != null){
+
                 callback(true);
-                socket.nickname = data; //Saving the nickname to the socket it self -> now it is a property of the socket
-                socket.userID = cryptoID;
+                socket.userID = user['userid']; //Saving the id to the socket it self -> now it is a property of the socket
+                socket.nickname = user['username']; //Saving the nickname to the socket it self -> now it is a property of the socket
+
+                /*socket.userID = cryptoID;*/
                 users[socket.nickname] = socket;
                 updateNicknames(); // Sending updated array with nicknames to all sockets
+
             } else {
                 callback(false);
             }
@@ -128,6 +127,9 @@ io.sockets.on('connection', function(socket){
         // Private message "listener".
 
         var msg = data['newMessage'].trim(); // trimming if user has put white space as first characters
+
+        console.dir(data);
+
         if(msg.substr(0,3) === '/w '){
             msg = msg.substr(3); // We need the next letters
             var ind = msg.indexOf(' ');
@@ -150,9 +152,11 @@ io.sockets.on('connection', function(socket){
                 callback('Error!  Please enter a message for your whisper.');
             }
         } else {
+
+
             // Create new document
             // Have set date to default now so we do not need to mention that
-            var newMsg = new Chat({msg: msg, nick: socket.nickname, msgFrom:0, msgTo:0});
+            var newMsg = new Chat({msg: msg, nick: socket.nickname, msgFrom: socket.userID, msgTo: data['toUser']});
             //Callback function for error
             newMsg.save(function(err){
                 if(err){
@@ -167,12 +171,25 @@ io.sockets.on('connection', function(socket){
     // Listening for selected user to message/chat with
     socket.on('choose chatter', function(data){
         console.log("Choose chatter: " + data);
+
+
         var conversation = Chat.find({});
 
-        conversation.find({ $and: [ { msgTo: 1 }, { msgFrom: 2 } ]}, function(err, conv) {
-            if (err) return console.error(err);
+        // Find specific conversation between user id's
+        //query.sort('-created').limit(8).exec(function(err, docs){
+        conversation.find({ $or: [
+            {'$and': [
+                { msgTo: data },
+                { msgFrom: socket.userID }
+            ]},
+            {'$and': [
+                { msgTo: socket.userID },
+                { msgFrom: data }
+            ]}
+        ]}).sort('-created').limit(8).exec(function(err, conv){
 
-            console.log(conv);
+            if (err) return console.error(err);
+            socket.emit('load old message', conv);
 
         });
 
