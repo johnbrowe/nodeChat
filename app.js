831e54607e6f025db385b330db90960b6e021884
John Høj Andreassen <jobbi_john@hotmail.com>
2014-08-13 12:38:34 +0100

    Comment clean up

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ var express = require('express'),
     server = require('http').createServer(app), //Create server object
     io = require('socket.io').listen(server), //Socket functionality which listens to the http server object
     mongoose = require('mongoose'),
-    // Keeping track of nicknames as objects, as well as being able to reference the single socket object so we can send to specific user sockets.
-    // Nickname will be the key to the users object and the values will be the sockets
+// Keeping track of nicknames as objects, as well as being able to reference the single socket object so we can send to specific user sockets.
+// Nickname will be the key to the users object and the values will be the sockets
     users = {};
 
 // Listen to port 3000
 server.listen(3000);
 
 //Database connect
-mongoose.connect('mongodb://localhost/nodeChat', function(err){
-    if(err){
+mongoose.connect('mongodb://localhost/nodeChat', function (err) {
+    if (err) {
         console.log("MongoDB: " + err);
     } else {
         console.log('DB connection successfull');
@@ -37,7 +37,6 @@ var userSchema = mongoose.Schema({
 });
 
 
-
 // Create model
 // Keep name singular -> it will be turned prural
 // 1. parameter name of collection/model
@@ -47,50 +46,42 @@ var User = mongoose.model('User', userSchema);
 
 
 // Create route with express
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 // Receive event (server-side)
 // Like document ready function
 // Every user has it's own socket -> (function(socket)).
-io.sockets.on('connection', function(socket){
-
-    //Grabb earlier messages when logged on
-    /*var query = Chat.find({});
-    query.sort('-created').limit(8).exec(function(err, docs){
-        if(err){
-            throw err;
-        } else {
-            socket.emit('load old message', docs);
-        }
-    });*/
+io.sockets.on('connection', function (socket) {
 
     //Listening for new user event
     //Callback parameter to send valid state data back to client
     //Sent back to function(data) in new user eventlistener
-    socket.on('new user', function(data, callback){
+    socket.on('new user', function (data, callback) {
 
         //Check if user is in database
         //This is a login without password for
         //simulating real users
         var userIsInDB = User.find({});
 
-        userIsInDB.findOne({username: data}, function(err, user) {
+        userIsInDB.findOne({username: data}, function (err, user) {
             if (err) return console.error(err);
 
             user.online = 1;
-            user.save(function(err) {
-                if (err) { return next(err); }
+            user.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
             });
 
         });
 
-        userIsInDB.findOne({username: data}, function(err, user) {
+        userIsInDB.findOne({username: data}, function (err, user) {
             if (err) return console.error(err);
 
             //If user is in database
-            if(user != null){
+            if (user != null) {
 
                 callback(true);
                 socket.userID = user['userid']; //Saving the id to the socket it self -> now it is a property of the socket
@@ -109,18 +100,18 @@ io.sockets.on('connection', function(socket){
 
 
     //Listening for send message event
-    socket.on('send message', function(data, callback){
+    socket.on('send message', function (data, callback) {
         // Private message "listener".
 
         var msg = data['newMessage'].trim(); // trimming if user has put white space as first characters
 
-        if(msg.substr(0,3) === '/w '){
+        if (msg.substr(0, 3) === '/w ') {
             msg = msg.substr(3); // We need the next letters
             var ind = msg.indexOf(' ');
-            if(ind !== -1){
+            if (ind !== -1) {
                 var name = msg.substring(0, ind); // Looking for first white space as after that the name should be finished
                 var msg = msg.substring(ind + 1);
-                if(name in users){
+                if (name in users) {
                     //emit using the socket we are sending the message to
                     users[name].emit('whisper', {msg: msg, nick: socket.nickname});
                     // If user has entered /w then it is a private message
@@ -142,8 +133,8 @@ io.sockets.on('connection', function(socket){
             // Have set date to default now so we do not need to mention that
             var newMsg = new Chat({msg: msg, nick: socket.nickname, msgFrom: socket.userID, msgTo: data['toUser']});
             //Callback function for error
-            newMsg.save(function(err){
-                if(err){
+            newMsg.save(function (err) {
+                if (err) {
                     throw err;
                 }
             });
@@ -153,7 +144,7 @@ io.sockets.on('connection', function(socket){
     });
 
     // Listening for selected user to message/chat with
-    socket.on('choose chatter', function(data){
+    socket.on('choose chatter', function (data) {
 
         var conversation = Chat.find({});
 
@@ -168,28 +159,30 @@ io.sockets.on('connection', function(socket){
                 { msgTo: socket.userID },
                 { msgFrom: data }
             ]}
-        ]}).sort('-created').limit(8).exec(function(err, conv){
+        ]}).sort('-created').limit(8).exec(function (err, conv) {
 
-            if (err) return console.error(err);
-            socket.emit('load old message', conv);
+                if (err) return console.error(err);
+                socket.emit('load old message', conv);
 
-        });
+            });
 
     });
 
     //When a user disconnects
-    socket.on('disconnect', function(data){
+    socket.on('disconnect', function (data) {
         //To disconnect them if they wisit the site, but do not enter anything we want to disconnect them from the socket.
-        if(!socket.nickname) return;
+        if (!socket.nickname) return;
         delete users[socket.nickname];
 
         var user = User.find({});
 
-        user.findOne({username: socket.nickname}, function(err, user) {
+        user.findOne({username: socket.nickname}, function (err, user) {
             if (err) return console.error(err);
             user.online = 0;
-            user.save(function(err) {
-                if (err) { return next(err); }
+            user.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
 
                 updateNicknames(); // Send the new list to all sockets so get the new list of users
 
